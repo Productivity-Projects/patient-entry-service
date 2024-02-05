67f04e2a1c7e890f0d3a5e9429d839baebb46733
shahidshaik92 <shahidshaik921@gmail.com>
2024-02-05 22:15:18 +0530

    renamed

--- src/main/java/com/hospital/entry/PatientEntryServiceApplication.java
+++ src/main/java/com/hospital/entry/PatientEntryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.hospital;
+package com.hospital.entry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PatientEntryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PatientEntryServiceApplication.class, args);
+		SpringApplication.run(PatientEntryServiceApplication.class, args);     
 	}
 
 }
--- src/test/java/com/hospital/entry/PatientEntryServiceApplicationTests.java
+++ src/test/java/com/hospital/entry/PatientEntryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hospital;
+package com.hospital.entry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
